ae18bd5e10ccfab68b5cd423b50174596257f2a6
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-04-21 11:34:40 +0200

    Add Background Color generated by Hex code

--- hex.js
+++ hex.js
@@ -1,1 +1,23 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+function generateColor() {
+    const btn = document.getElementById("btn");
+    const color = document.querySelector('.color');
+
+    btn.addEventListener('click', () => {
+        let hexCode = '#';
+
+        for (let index = 0; index < 6; index++) {
+            hexCode += hex[getRandom()];
+        }
+        document.body.style.backgroundColor = hexCode;
+        color.textContent = hexCode;
+    })
+}
+
+
+function getRandom() {
+    return Math.floor(Math.random() * hex.length);
+}
+
+generateColor();
